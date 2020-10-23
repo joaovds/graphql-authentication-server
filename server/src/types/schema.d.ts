371c980342c8c2a781ab3bfd312866824e895aea
João Victor <jv782063@gmail.com>
2020-10-23 17:13:29 -0300

    adds type creator to the schema, and already types the current schema

---
+++ server/src/types/schema.d.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+// tslint:disable
+// graphql typescript definitions
+
+declare namespace GQL {
+  interface IGraphQLResponseRoot {
+    data?: IQuery;
+    errors?: Array<IGraphQLResponseError>;
+  }
+
+  interface IGraphQLResponseError {
+    /** Required for all errors */
+    message: string;
+    locations?: Array<IGraphQLResponseErrorLocation>;
+    /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+    [propName: string]: any;
+  }
+
+  interface IGraphQLResponseErrorLocation {
+    line: number;
+    column: number;
+  }
+
+  interface IQuery {
+    __typename: 'Query';
+    users: Array<IUser>;
+  }
+
+  interface IUser {
+    __typename: 'User';
+    id: string;
+    name: string;
+    email: string;
+  }
+}
+
+// tslint:enable
