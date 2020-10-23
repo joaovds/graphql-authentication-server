90c388e2a547dd3e4a733e4433359abb79094acb
João Victor <jv782063@gmail.com>
2020-10-23 16:52:09 -0300

    add graphql-yoga and make the server run

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+type Query {
+  hello: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+// eslint-disable-next-line no-console
+server.start(() => console.log('Server is running on http://localhost:4000'));
