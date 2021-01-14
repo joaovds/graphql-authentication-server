3080724be544d5683d971e88b231089124157f22
João Victor da Silva <jv782063@gmail.com>
2021-01-14 19:37:51 -0300

    adds and configures react-router-dom

---
+++ web/src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import Login from './pages/Login';
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
